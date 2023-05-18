ccb9e467678f66470a0ae1cb0d1dc059bc9475a3
UtkuOzkutucu <utku__ozkutucu@hotmail.com>
2023-05-18 21:40:19 +0300

    Update arsa.py
    Fonksiyonlarin Belirlenmesi

--- arsa.py
+++ arsa.py
@@ -4,3 +4,15 @@ global Kirsal
 Deniz_Kenari = 1.6      #Bolge 1
 Sehir_Merkezi = 1.2     #Bolge 2
 Kirsal = 0.8            #Bolge 3
+
+def ArsaCemberCevre(Yaricap):
+    return 6 * Yaricap
+
+def ArsaDikdortgenCevre(Dikey,Yatay):
+    return 2 * (Dikey + Yatay)
+
+def CemberFiyat(ArsaCemberCevre,Bolge_Katsayisi):
+    return 1000* ArsaCemberCevre * Bolge_Katsayisi
+
+def DikdorgenFiyat(ArsaDikdorgenCevre,Bolge_Katsayisi):
+    return ArsaDikdorgenCevre * Bolge_Katsayisi * 1000
