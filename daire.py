86a0bae1e27d4d93542cb5cf7a6a65314a8d4b0f
UtkuOzkutucu <utku__ozkutucu@hotmail.com>
2023-05-18 21:48:47 +0300

    Update daire.py
    Fonksiyon belirleme

--- daire.py
+++ daire.py
@@ -1,3 +1,9 @@
 Ara_Kat_Katsayisi = 2       #Katsayisi 1
 Ust_Kat_Katsayisi = 1.6     #Katsayisi 2
 Zemin_Kat_Katsayisi = 0.9   #Katsayisi 3
+
+def hesapla_daire_alani(Dikey,Yatay):
+    return Dikey * Yatay    
+
+def hesapla_fiyat(kat_katsayisi, daire_alani):
+    return kat_katsayisi * daire_alani * 5000
